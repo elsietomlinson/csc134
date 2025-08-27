355d441df05a41e25e720c57722715836b02fbc5
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-08-27 15:29:53 +0000

    added initial code

---
+++ M1HW1.cpp
@@ -0,0 +1,21 @@
+/* 
+csc 134
+M1HW1
+elsie tomlinson
+8/27/2005
+purpose: write program giving name and release date of movie
+*/
+//magic words
+#include <iostream> 
+using namespace std;
+
+int main() {
+    //declare variables
+    string item_name = "Shrek.";
+    int year_released = 2001;
+    double gross_income = 427;
+
+
+// end program
+return 0;
+}
