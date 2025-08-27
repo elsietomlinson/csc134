773d04c4ad0a964b1079e621c5a2be7635af360c
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-08-27 14:29:46 +0000

    started code

---
+++ M1L1.cpp
@@ -0,0 +1,20 @@
+/*
+csc 134
+m1lab
+elsie tomlinson
+8/27/2025
+purpose: the "kamkwat" sales program.
+*/
+
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+  // start program
+  string item = "kamkwat"; // replaced with your item
+  cout << "Welcome to the" << item << "store!" << endl;
+
+  //end program 
+  return 0;
+}
