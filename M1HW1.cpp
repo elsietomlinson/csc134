c7ab243fb780c124769f7cd6a13089f9650eb273
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-08-27 15:36:41 +0000

    added memorable quote

--- M1HW1.cpp
+++ M1HW1.cpp
@@ -11,14 +11,15 @@ using namespace std;
 
 int main() {
     //declare variables
-    string item_name = "Shrek.";
+    string item_name = "'Shrek'.";
     int year_released = 2001;
     double gross_income = 427;
+
 // actual text
 cout << "The motion picture " << item_name << endl;
 cout << "was released in the year " << year_released << "," << endl; 
 cout << " and it grossed " << gross_income << " million dollars."<< endl;
-
+cout << "some memorable quotes include,'What are you doing in my swamp?!'" << endl;
 // end program
 return 0;
 }
