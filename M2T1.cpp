85acd243e7609f972445dc7d7f9b0f5507ff11e5
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-03 14:42:11 +0000

    asked for informatoin

--- M2T1.cpp
+++ M2T1.cpp
@@ -15,10 +15,13 @@ int main()  {
     string item = "kamkwats";
     double cost_per = 0.99;
     int amount = 20;
+    //variables for user input
+    int amount_purcahsed;
+    double total_cost;
     //Greet the user
 cout << "Hello! Welcome to our " << item << "store." << endl;
     // ask for information
-
+cout << "Each of the " << item << "cost $" << cost_per << "." << endl;
     //do some processing 
     
     //output the answer
