598374647937f8c04c9f32d100957d360123d5c8
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-03 14:44:27 +0000

    fixed misspelling

--- M2T1.cpp
+++ M2T1.cpp
@@ -23,7 +23,7 @@ cout << "Hello! Welcome to our " << item << " store." << endl;
     // ask for information
 cout << "Each of the " << item << " cost $" << cost_per << "." << endl;
 cout << endl; 
-cout << "How mant kamkwats would you like to buy?" << endl;
+cout << "How many kamkwats would you like to buy?" << endl;
     //do some processing 
     
     //output the answer
