49291cc48b96dacdc25786d4f8cc0e8d302d9247
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-03 14:39:34 +0000

    psudo codef

--- M2T1.cpp
+++ M2T1.cpp
@@ -9,9 +9,19 @@ m2t1 apple store w/ input
 
 #include <iostream>
 using namespace std;
-int main()  
-{
+int main()  {
 
+    //declare variables
+    string item = "kamkwats";
+    double cost_per = 0.99;
+    int amount = 20;
+    //Greet the user
+cout << "Hello! Welcome to our " << item << "store." << endl;
+    // ask for information
+
+    //do some processing 
+    
+    //output the answer
 
 return 0;
 }
