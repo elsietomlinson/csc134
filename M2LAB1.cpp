03ca8a47b30b747e1146eb5c7d6b2e69f61cdf0d
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-08 14:41:44 +0000

    added how to calculate cost and profit

--- M2LAB1.cpp
+++ M2LAB1.cpp
@@ -11,6 +11,7 @@ using namespace std;
 int main() {
     // set up the variables 
 double length, width, height; // size of the crate
+double volume;
 double cost_to_make, charge_to_customer; // pricing
 double profit;
 
@@ -19,9 +20,16 @@ const double CHARGE_PER_CUBIC_FOOT = 0.5; // relates to charge
 
 
     // get crate information
-
+cout << "Welcome to the crate program. " << endl; 
+cout << "Crate Length: " << endl;
+cin >> length;
+cout << "Crate Width: " << endl;
+cin >> width; 
     // calculate cost and prices
-
+volume = length * width * height;
+cost_to_make = volume * COST_PER_CUBIC_FOOT;
+charge_to_customer = volume * CHARGE_PER_CUBIC_FOOT;
+profit = charge_to_customer - cost_to_make;
     //display the results
 
     return 0; //no errors
