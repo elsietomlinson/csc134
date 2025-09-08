3026d550e571c7d0631544d173778138a904fc43
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-08 14:34:34 +0000

    started design fo code

---
+++ M2LAB1.cpp
@@ -0,0 +1,28 @@
+/*
+elsie tomlinson 
+csc-134 
+M2 LAB 1 - case study with crates
+09/08/2025
+*/ 
+
+#include <iostream> 
+using namespace std;
+
+int main() {
+    // set up the variables 
+double length, width, height; // size of the crate
+double cost_to_make, charge_to_customer; // pricing
+double profit;
+
+const double COST_PER_CUBIC_FOOT = 0.23; // relates to cost
+cost double CHARGE_PER_CUBIC_FOOT = 0.5; // relates to charge
+
+
+    // get crate information
+
+    // calculate cost and prices
+
+    //display the results
+
+    return 0; //no errors
+}
