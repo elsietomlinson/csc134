7456a1d1cc6d6593b427bce9f0a56585473c0613
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-15 14:40:02 +0000

    starting code

---
+++ M2HW1.cpp
@@ -0,0 +1,19 @@
+/* 
+csc-134
+elsie tomlinson 
+m2hw1
+09/15/2025
+*/
+// magic words
+#include <iostream> 
+#include <iomanip>
+using namespace std;
+
+int main () {
+
+    cout << fixed << setprecision (2)
+    
+    cout << "QUESTION ONE" << endl;
+
+string lname
+}
