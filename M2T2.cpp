eb1200b0a53004efb841bac703cf989bd605078d
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-15 00:30:14 +0000

    started code

---
+++ M2T2.cpp
@@ -0,0 +1,29 @@
+/*
+elsie tomlinson 
+csc-134 
+M1T2 store with sales tax
+09/14/2025
+*/
+// must include price before tax
+// the amount of tax
+// total after tax
+#include <iostream>
+#include <iomanip> // neccessary to make it look pretty with the decimals
+using namespace std;
+
+int main() {
+//variables 
+double food_price = 17.99;
+string storename = "Ranchoros!";
+string fooditem = "Racheros";
+
+
+// actual order
+cout << "welcome to the " << storename << endl;
+cout << "One order of " << fooditem << " is " << endl; 
+cout << food_price << endl;
+
+
+
+
+}
