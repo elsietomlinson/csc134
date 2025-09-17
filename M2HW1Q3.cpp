193dec7d8926e3d19442df2dacd0bff93d378a9b
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-17 03:22:56 +0000

    added greeting

--- M2HW1Q3.cpp
+++ M2HW1Q3.cpp
@@ -15,7 +15,14 @@ const int slices_per_guest = 3;
 int pizza, slices_per_pizza, party_goers;
 // how many pizzas were ordered
 cout << "Welcome to Chuckee Cheez! How many pizza's would you like to order today?" << endl;
-
+cin >> pizza;
+cout << endl;
+// how many slices do they wnat
+cout << "How many slices would you like?" << endl;
+cin >> slices_per_pizza;
+cout << endl;
+// how many guests are there going to be? 
+cout << "How big is your party?" 
 
 
 
