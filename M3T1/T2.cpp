5a05f6d5dd1aece51372b17753b18267dc1096c8
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-17 14:22:40 +0000

    initial commit

---
+++ M3T1/T2.cpp
@@ -0,0 +1,20 @@
+/* elsie tomlinson
+m3t1/t2
+csc-134
+09/17/2025
+area of a rectangle
+*/
+#include <iostream> 
+using namespace  std;
+
+int main () { 
+
+
+
+
+
+
+
+
+    return 0; // no errors
+}
