b7127c316f85471f22df030bc2ce86d8177e4853
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-17 03:41:24 +0000

    initial commit

---
+++ M2HW1q4.cpp
@@ -0,0 +1,26 @@
+/*
+elsie tomlinson
+csc-134
+M2hw1q4
+string ftcc thingt
+
+*/
+
+#include <iostream>>
+using namespace std;
+int main() {
+// strings
+
+string letsgo = "Let's go";
+string school = "FTCC";
+string team = "Trojans";
+string cheerone = letsgo + " " + school;
+string cheertwo = letsgo + " " + team;
+//put it together in out put
+cout << letsgo << endl;
+cout << letsgo << endl; 
+cout << cheerone << endl; 
+cout << cheertwo << endl;
+
+    return 0;
+}
