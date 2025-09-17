48fc9a93fc9b47a89ad14e51317b9d8e9615af56
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-17 03:19:45 +0000

    started code

---
+++ M2HW1Q3.cpp
@@ -0,0 +1,26 @@
+/*
+elsie tomlinson 
+csc-134
+m2hw1 q3
+pizza party program
+
+*/
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    // variables 
+const int slices_per_guest = 3;
+int pizza, slices_per_pizza, party_goers;
+// how many pizzas were ordered
+cout << "Welcome to Chuckee Cheez! How many pizza's would you like to order today?" << endl;
+
+
+
+
+
+
+
+    return 0; // no errors
+}
