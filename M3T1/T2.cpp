c9891e1b4855cfd7bef23712c4ff2c39cabd5796
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-17 14:29:03 +0000

    started variables

--- M3T1/T2.cpp
+++ M3T1/T2.cpp
@@ -8,6 +8,25 @@ area of a rectangle
 using namespace  std;
 
 int main () { 
+//get info on two rectangles
+double length1, length2, width1, width2;
+double area1, area2;
+cout << "Enter the length and width of two rectangles." << endl;
+cout << "First rectangle." << endl;
+cout << "Length:" << endl;
+cin >> length1;
+cout << "Width: " << endl;
+cin>> width1;
+cout << endl;
+cout << "Second rectangle." << endl;
+cout << "Length: " << endl; 
+cin >> length2;
+cout << "QWidth: " << endl;
+cin >> width2;
+//calculate the area
+area1 = length1 * width1;
+area2 = length2 * width2; 
+//print the output
 
 
 
