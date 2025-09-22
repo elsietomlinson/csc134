118a525da0ee5f8734aa55b38a0f87517c3f98bb
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-22 14:51:01 +0000

    cleaned m3t2

--- modulethree/m3t1/m3t2.cpp
+++ modulethree/m3t1/m3t2.cpp
@@ -19,29 +19,32 @@ cout << "Roll the die!"<< endl;
 
 // craps cycle
 cout << num << endl;
-int roll1 = 2;
-int roll2 = 5;
+int roll1 = 3;
+int roll2 = 4;
 int sum = roll1 + roll2;
-
+cout << "The first roll was a " << roll1 << "." << endl;
+cout << "The second roll was a " << roll2 << "." << endl; 
 
 if (sum == 7) { 
     cout << "Lucky Seven -- you win!" << endl;
 }
 else { 
     cout << "Did not roll a seven." << endl;
-    
+
 }
-}
+
 
 
 
 return 0;    
 }
-
 //define helper function
-int roll () {
-    //roll a six sided die
-    return 6;
 
+    //roll a six sided die
+ int roll() {
+ 
+    return 0; 
 }
+
+
 
