87db8c8800e8e730150d4cfa2fa1b754d4a2300a
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-22 14:21:25 +0000

    started code

---
+++ modulethree/m3t1/m3t2.cpp
@@ -0,0 +1,22 @@
+/*
+elsie tomlinson 
+m3t2
+09/22/2025
+csc-134 
+*/
+#include <iostream>
+#include <cmath>
+#include <ctime>
+using namespace std;
+int main () {
+cout << "Random number generator." << endl;
+srand(time(0));
+int number1 = rand();
+
+
+
+
+
+return 0;    
+}
+
