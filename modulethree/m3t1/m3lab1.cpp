973f4319dc2c5ab0e06305dec51bd4ba21b9fa86
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-24 14:21:39 +0000

    skeleton code

--- modulethree/m3t1/m3lab1.cpp
+++ modulethree/m3t1/m3lab1.cpp
@@ -7,9 +7,20 @@ meow meow meow meow making a game
 */
 #include <iostream> 
 using namespace std;
+// declare the "rooms" that you're going to be using   
+void bedroom(); // starting room
+void forest (); // outside
+void store (); // another place to be 
+void gameOver (); // ending 
+void victory (); // winning 
 int main () {
-
-
+cout << "Welcome to this thrilling adventure!" << endl;
+cout << endl;
+void bedroom () 
+{
+    out << "Welcome to your childhood bedroom." << endl;
+cout << "You need to go buy some groceries, head to the store!" << endl;
+}
 
 
 
