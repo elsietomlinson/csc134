1834d22d263899595967fb1f4df92ad359b09992
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-24 14:30:29 +0000

    saving, it's bugged though

--- modulethree/m3t1/m3lab1.cpp
+++ modulethree/m3t1/m3lab1.cpp
@@ -18,7 +18,18 @@ cout << "Welcome to this thrilling adventure!" << endl;
 cout << endl;
 void bedroom (); {
 cout << "Welcome to your childhood bedroom." << endl;
-cout << "You need to go buy some groceries, head to the store!" << endl;
+cout << "Do you choose door one or door two?" << endl; 
+int choice;
+cin >> choice; 
+if (choice == 1)
+{
+    cout << "Brave soul leaving your home! Don't run into anyone you know!" << endl; 
+    store(); // go to store
+}
+else { 
+    cout << "Go bed rot on Tik Tok till your parents call you!" << endl;
+    gameOver();
+} 
 }
 
 
