885040335413378711f5e71bada2f1f6827d2a83
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-24 15:25:22 +0000

    CODE WASN"T WORKING CAUSE I CNAT ORGANIXE

--- modulethree/m3t1/m3lab1.cpp
+++ modulethree/m3t1/m3lab1.cpp
@@ -9,18 +9,32 @@ meow meow meow meow making a game
 using namespace std;
 // declare the "rooms" that you're going to be using   
 void bedroom(); // starting room
-void forest(); // outside
 void store(); // another place to be 
 void gameOver(); // ending 
 void victory(); // winning 
+
+
+// main()
 int main () {
-cout << "Welcome to this thrilling adventure!" << endl;
-cout << endl;
-    return 0; 
+    cout << "Welcome to this thrilling adventure!" << endl;
+    cout << endl;
+    cout << "You're at home. Do you:" << endl;
+    cout << "1. Go to the store" << endl;
+    cout << "2. Stay home" << endl;
+
+    int choice;
+    cin >> choice; 
+    if (choice == 1)
+    {
+        cout << "Brave soul leaving your home! Don't run into anyone you know!" << endl; 
+        store(); // go to store
+        victory();// you win!
+    }
+    else if (choice == 2) { 
+        gameOver();// game ends and you bed rot and don't go out 
+    }
+    return 0; // end of main
 } 
-
-
-
 
 // end of main beginning of new function
 // DEFINE all the other functions here, in this format:
@@ -28,28 +42,19 @@ cout << endl;
 void myfunction() {}
 */
 
-  void bedroom() {
-cout << "Welcome to your childhood bedroom." << endl;
-cout << "Do you choose to venture out? (Type 1 to leave)" << endl; 
-int choice;
-cin >> choice; 
-if (choice == 1)
-{
-    cout << "Brave soul leaving your home! Don't run into anyone you know!" << endl; 
-    store(); // go to store
-}}
- void store(); {
+void bedroom() {
+    cout << "Welcome to your childhood bedroom." << endl;
+    cout << "Do you choose to venture out? (Type 1 to leave)" << endl; 
+}
 
-    
-    cout << "Welcome to the store! You have ventured out of your humble abode!" << endl;
-    cout << "Now you must do the grocery shopping for the week or you'll starve!" << endl; 
-
-  }
-if (choice == 2) { 
-    gameOver();// game ends and you bed rot and don't go out 
-    }
-void gameOver(); {
+void gameOver() {
     cout << "Go bed rot on Tik Tok till your parents call you!" << endl;
 }
     //game over function
-
+ void store() {
+    cout << "Welcome to the store! You have ventured out of your humble abode!" << endl;
+    cout << "Now you must do the grocery shopping for the week or you'll starve!" << endl;
+}
+void victory() {
+    cout << "You made it to the store, you win cause you went grocery store." << endl;
+}
