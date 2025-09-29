8427890db8af71bb822b047181bb898766918e51
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-29 14:38:12 +0000

    started

--- modulethree/m3t1/m3la2.cpp
+++ modulethree/m3t1/m3la2.cpp
@@ -18,8 +18,24 @@ void combat();
 
 
 int main () {
-
-
+letter_grader(); 
+//combat();
 
 return 0;
+}
+
+// define other fucntions under main
+void letter_grader() {
+    // input a number grade
+    double num_grade; 
+    string letter_grade;
+    cout << "Enter a number grade 0-100:" << endl;
+        // respond with a letter grade
+    cin >> num_grade;
+
+    // create if statements 
+    letter_grade = "A";
+    //output answer
+    cout << "A number gade of" << num_grade << " is a(n) " << letter_grade;
+    cout << endl << endl;
 }
