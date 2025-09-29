4492c91fdbb450a1a5db46d2f32adc6f8c135bb3
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-29 14:31:15 +0000

    started m3lab2

---
+++ modulethree/m3t1/m3la2.cpp
@@ -0,0 +1,25 @@
+/*
+elsie tomlinson 
+csc-134
+module three lab 2
+putting in numerical grades and getting a letter grade back (changed to game mechanics test)
+*/
+
+
+#include <iostream> 
+#include <cstdlib> // for randon
+#include <ctime> // to seed random
+using namespace std; 
+// delcaring functions
+// to make things easier, write in a function 
+void leter_grader();
+void combat();
+
+
+
+int main () {
+
+
+
+return 0;
+}
