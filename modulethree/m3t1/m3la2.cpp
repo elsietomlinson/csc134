35c4d7888c3311e1fd7d4fe80271342c9febf330
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-09-29 14:45:17 +0000

    addded if statements

--- modulethree/m3t1/m3la2.cpp
+++ modulethree/m3t1/m3la2.cpp
@@ -26,16 +26,37 @@ return 0;
 
 // define other fucntions under main
 void letter_grader() {
+   
     // input a number grade
     double num_grade; 
     string letter_grade;
+    // constants for grade points
+    const double A_GRADE = 90;
+    const double B_GRADE = 80;
+    const double C_GRADE = 70;
+    const double D_GRADE = 60;
     cout << "Enter a number grade 0-100:" << endl;
-        // respond with a letter grade
+   
+    // respond with a letter grade
     cin >> num_grade;
 
     // create if statements 
+  if (num_grade <= 90) {
     letter_grade = "A";
+  }
+  else if (num_grade <= B_GRADE) {
+    letter_grade + "B";
+  }
+   else if (num_grade <= C_GRADE) {
+    letter_grade = "D";
+   }
+   else if (num_grade <= D_GRADE) {
+    letter_grade = "D";
+   }
+   else {
+    letter_grade = "F";
+   }
     //output answer
-    cout << "A number gade of " << num_grade << " is a(n) " << letter_grade;
+    cout << "A number gade of " << num_grade << " is:  " << letter_grade;
     cout << endl << endl;
 }
