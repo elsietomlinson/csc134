9f12cccdbf011385d940fe0312a7b9b7ae6fff13
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-10-06 14:37:24 +0000

    started m4 lab 1

---
+++ modulethree/m3t1/M4LAB1.cpp
@@ -0,0 +1,27 @@
+/* 
+elsie tomlinson
+csc-134
+bonus but not bonus assignment 
+ai has got free pass for this i guess
+*/
+#include <iostream> 
+using namespace std;
+    int main () {
+int health = 50; 
+int Maxhealth = 100: 
+while (health < Maxhealth)
+{
+    health = health + 10
+    cout << "Health: " << health << "/" << Maxhealth << endl;
+}
+
+cout << "Fully recovered: " << endl;
+
+
+
+
+
+
+
+        return 0;
+    }
