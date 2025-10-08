64f0a85fb1bf466fd7cedfc1193d451d396f0e31
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-10-08 15:35:08 +0000

    started m3hw question 2

---
+++ modulethree/m3t1/m3hwq2.cpp
@@ -0,0 +1,30 @@
+/* 
+elsie tomlinson
+m3hw 
+q2
+gold tier 
+meow
+*/
+#include <iostream> 
+#include <iomanip> // to make it PRETTY
+using namespace std; 
+int main () {
+
+cout << "Question 2;" << endl;
+// set up thine variables
+double price; 
+string storename = "Fuel for your physical manifestation.";
+string fooditem; 
+int hereortogo; // for here or to go man
+cout << fixed << setprecision (2); 
+
+// do the thing bro
+cout << "Welcome to " << storename << endl;
+cout << "What would you like to eat today?" << endl;
+cin >> fooditem;
+cout << endl; 
+cout 
+
+
+    return 0;
+}
