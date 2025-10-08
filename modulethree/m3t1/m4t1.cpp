14badc0408380d40dc66ab2f751fab815425f9ba
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-10-08 15:03:19 +0000

    m4t1 run

---
+++ modulethree/m3t1/m4t1.cpp
@@ -0,0 +1,19 @@
+/*
+elsie tomlinson
+m4t1 
+10/08/2025
+*/
+#include <iostream> 
+using namespace std;
+
+int main() {
+int number = 0;
+while (number < 5) {
+    cout << "Hello!" << endl;
+
+}
+
+cout << "That's all!" << endl;
+
+    return 0;
+}
