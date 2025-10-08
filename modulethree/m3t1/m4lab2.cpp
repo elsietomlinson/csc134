5df2bded71f47b45f20f2f69226f4d5de3341ccf
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-10-08 14:47:50 +0000

    added it in a column

--- modulethree/m3t1/m4lab2.cpp
+++ modulethree/m3t1/m4lab2.cpp
@@ -11,19 +11,24 @@ using namespace std;
 int main () {
 
 //declare your variables
-int length, width;
+int length, height;
 string tile = " ✨"; // any character/ emoji
 
 // example 1L print horixontally (ROW)
-for (int i=0; i < length; i++) {
-    cout << tile;
-}
+    for (int i=0; i < length; i++) {
+        cout << tile;
+    }
 cout << endl;
 //example 2 samte thing witha while loop
 int count = 0;
-while (count < length) {
-    cout << tile;
-    count ++;
+    while (count < length) {
+        cout << tile;
+        count ++;
+    }
+cout << endl;
+//ex3 for loop but in a column now
+for (int i = 0; i < height; i++) {
+    cout << tile << endl;
 }
-return 0;
+return 0; // no errors 
 }
