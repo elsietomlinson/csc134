acd35ca7aa4b1f3be410ceb647188eff013c9535
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-10-10 21:14:28 +0000

    started m3hwq4

---
+++ m3hwq4.cpp
@@ -0,0 +1,27 @@
+/* 
+elsie tomlinson
+csc-134 
+gold tier
+m3hw question 1 
+*/
+#include <iostream>
+#include <ctime>
+#include <cmath>
+using namespace std; 
+int main () {
+cout << "QUESTION 4" << endl; 
+cout << endl; 
+// random number seed
+int seed = time(0);
+srand(seed);
+
+
+
+
+
+
+
+
+
+    return 0;
+}
