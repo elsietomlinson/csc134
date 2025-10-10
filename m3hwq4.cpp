6aaddde8085f09df07638011a7d9c64c3ec2c4f2
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-10-10 23:22:57 +0000

    finished m3hw q 4

--- m3hwq4.cpp
+++ m3hwq4.cpp
@@ -15,13 +15,26 @@ cout << endl;
 int seed = time(0);
 srand(seed);
 
+//variables
+const int MAX = 9;
+int digit1, digit2, usersum, total; 
 
+//random numbers 
+digit1 = (rand() % MAX) + 1;
+digit2 = (rand() % MAX) + 1;
+total = digit1 + digit2;
 
-
-
-
-
-
+//print out all numbers
+cout << "Let's practice some math! What does " << digit1 << " + " << digit2 << " = " << endl; 
+cin >> usersum;
+// now tell them if it is correct or not
+if (usersum  == total) {
+    cout << usersum << " is correct!" << endl; 
+}
+if (usersum != total) {
+    cout << usersum << " is not right." << endl;
+    cout << "Sorry, try again." << endl; 
+}
 
     return 0;
 }
