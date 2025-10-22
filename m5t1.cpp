628e791a3d43e0ca26853e22751ef722a5049d4b
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-10-22 14:41:07 +0000

    started m5t1

---
+++ m5t1.cpp
@@ -0,0 +1,28 @@
+/*
+elsie tomlinson
+csc 134 
+m5 t1
+all about functions */
+//purpose demoand value returning fucntions
+#include <iostream>
+using namespace std;
+// function declarations 
+void say_hello(); //says hi
+
+int get_the_answer(); // provides the anser
+
+double double_a_number(); // num times two 
+
+
+int main(); {
+    double my_num;
+    int another_num;
+    cout << "welcome to a program" << endl; 
+    cout << " Please enter a number (with or without decimal place)" << endl; 
+    cin >> my_num
+    my_num = my_num * 2;
+    cout << "Double the number is: " << my_num << endl; 
+    
+    return 0;
+}
+
