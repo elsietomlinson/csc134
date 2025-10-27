b7367be8932c8da91aff460886d31dded43fb378
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-10-27 14:23:35 +0000

    setting up main so i can write my functions

--- pystuff/m4hw1.cpp
+++ pystuff/m4hw1.cpp
@@ -9,23 +9,41 @@ void part1();
 void part2();
 void part3();
 void part4();
+// magic words
 #include <iostream> 
 using namespace std;
-int main(){
-    int selection; //  input
-    bool keep_going = true;
+    int main(){
+        int selection; //  input
+        bool keep_going = true;
 
-    while (true == keep_going) {
-        cout << "Select Question part you would like to see." << endl; 
-        cout << "Type 1, 2, 3, 4, or 0 to end." << endl;
-        cin >> selection; 
-if (selection == 1) {
-    part1(); 
-    cout << endl;
-}
-else if (selection == 2) {
-    part2();
-    cout << endl; 
-}
+        while (true == keep_going) {
+            cout << "Select Question part you would like to see." << endl; 
+            cout << "Type 1, 2, 3, 4, or 0 to end." << endl;
+            cin >> selection; 
+    if (selection == 1) {
+        part1(); 
+        cout << endl;
     }
+    else if (selection == 2) {
+        part2();
+        cout << endl; 
+    }
+    else if (selection == 3) {
+        part3();
+        cout << endl; 
+    }
+    else if (selection == 4) {
+        part4();
+    }
+    else if (selection == 0) {
+        cout << "Bye! See ya later!" << endl; 
+        cout << endl; 
+        keep_going = false; 
+    }
+    else {
+        cout << "Type the options I gave you." << endl;
+    }
+        }
+    
+    return 0;
 }
