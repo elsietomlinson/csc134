3376159894fe7c263071b37fa9a0e0f315734ace
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-11-03 16:08:08 +0000

    fixed my main to have a menu

--- .vscode/m5lab2.cpp
+++ .vscode/m5lab2.cpp
@@ -29,24 +29,9 @@ using namespace std;
 
 int main()
 {
-
+menu_main();
 	
-   double length,    // The rectangle's length
-          width,     // The rectangle's width
-          area;      // The rectangle's area
-          
-   // Get the rectangle's length.
-   length = getLength();
    
-   // Get the rectangle's width.
-   width = getWidth();
-   
-   // Get the rectangle's area.
-   area = getArea(length, width);
-   
-   // Display the rectangle's data.
-   displayData(length, width, area);
-          
    return 0;
 }
 
