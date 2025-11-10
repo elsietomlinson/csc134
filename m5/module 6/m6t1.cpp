1d6c4f7cc9fa57e099bacb85c6b1fca8706bfbcb
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-11-10 15:36:12 +0000

    started m6t1

---
+++ m5/module 6/m6t1.cpp
@@ -0,0 +1,17 @@
+/* elsie tomlisnon
+m6t1
+11/10/2025
+ meow meow
+ buckshot roulette? idk what that is, never played it. 
+ */
+#include <iostream>
+using namespace std; 
+int main() {
+
+
+
+
+
+    
+    return 0;
+}
