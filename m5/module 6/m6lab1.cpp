56d9b04572295bf43685d6e33a3bd264f6e4b4cf
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-11-12 15:32:05 +0000

    satarted my m5hw

---
+++ m5/module 6/m6lab1.cpp
@@ -0,0 +1,20 @@
+/* 
+elsie tomlinson 
+m6lab1 
+11/12/2025
+meow meow moew 
+*/
+#include <iostream> 
+using namespace std; 
+int main() {
+
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ m5hw1.cpp
@@ -0,0 +1,54 @@
+/*
+elsie tomlinson 
+m5h3w
+11/12/2025
+*/
+#include <iostream> 
+#include <iomanip>
+using namespace std;
+// hey here are my quesiton parts
+void Q1();
+void Q2();
+void Q3();
+void Q4();
+void Q5();
+void Q6();
+
+int main() {
+
+int selection; 
+bool keep_going = true;
+
+cout << "SELECT QUESTION" << endl; 
+cout << "1. Question 1" << endl;
+cout << "2. Question 2" << endl;
+cout << "3. Question 3" << endl;
+cout << "4. Question 4" << endl; 
+cout << "5. Question 5" << endl;
+cout << "6. Question 6" << endl; 
+cout << "0. Quit. " << endl; 
+cout << "What's it gonna be?" << endl; 
+cin >> selection; 
+// if statements to create main menu
+    if (selection = 1) {
+        Q1();
+    }
+    else if (selection = 2) {
+        Q2();
+    }
+    else if (selection = 3) {
+        Q3();
+    }
+    else if (selection = 4) {
+        Q4();
+    }
+    else if (selection = 5) {
+        Q5();
+    }
+    else if (selection = 6) {
+        Q6();
+    }
+
+
+    return 0; 
+}
