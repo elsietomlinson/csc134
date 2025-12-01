f1c0e3f0e1650fe9e504ca7954893ddeb456833e
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-12-01 15:33:11 +0000

    base code for m7t1

---
+++ m7t1.cpp
@@ -0,0 +1,40 @@
+/* elsie tomlinson
+ m7t1 
+ 12/01/2025
+ meow
+ */
+#include <iostream>
+ using namespace std;
+// Next time we'll put the class in a separate file
+class Restaurant {
+  private:
+    string name;    // the name
+    double rating;  // 0 to 5 stars
+
+  public:
+	// constructor 
+	Restaurant(string n, double r) {
+		name = n;
+		rating = r;
+	}
+	// getters and setters
+    void setName(string n) {
+        name = n; 
+    }
+    void setRating(double r) {
+        rating = r;
+    }
+    string getName() const {
+        return name;
+    }
+    double getRating() const {
+        return rating;
+    }
+  
+};
+ int main () {
+
+  cout << "M7T1 - Restaurant Reviews" << endl;
+
+    return 0;
+ }
