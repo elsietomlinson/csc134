b5701a3858a43471a755c98ac4265e798f21203b
Elsie Tomlinson <tomlinse3044@student.faytechcc.edu>
2025-12-03 15:16:30 +0000

    started m7 hw

---
+++ m7hw.cpp
@@ -0,0 +1,22 @@
+/* 
+elsie tomlinson
+m7hw
+gold tier
+12/3/2025
+*/
+#include <iostream> 
+using namespace std; 
+
+void intro();
+int main () {
+
+
+
+
+    return 0;
+}
+
+void intro()  {
+
+    cout << "Welcome to the mini project!" << endl; 
+}
